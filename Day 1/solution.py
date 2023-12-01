88bdc16a2e7f07fc805edc606515b9f65614da1e
Keith Dao <keith.dao.248@gmail.com>
2023-12-01 15:28:09 +1000

    Adds day 1 solver.

---
+++ Day 1/solution.py
@@ -0,0 +1,92 @@
+"""
+Day 1 solution.
+"""
+import sys
+
+
+class Solver:
+    """Day 1 solver."""
+
+    mapping = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
+
+    def __init__(self, filepath: str = "input.txt"):
+        self.filepath = filepath
+
+    def general_solve(self, include_word_numbers: bool = False) -> int:
+        """Solving algorithm for part 1 and part 2."""
+
+        def substr_to_num(substr: list[str]) -> int:
+            """
+            Converts the last substr to a number if valid, else returns -1.
+            """
+            num = -1
+            for length in range(3, min(6, len(substr))):
+                if (word := "".join(substr[-length:])) not in self.mapping:
+                    continue
+                num = self.mapping[word]
+            return num
+
+        def line_to_num(line: str) -> int:
+            first = last = -1
+            chars: list[str] = []
+            for c in line:
+                if c.isdigit():
+                    num = int(c)
+                    chars.clear()
+                elif not include_word_numbers:
+                    continue
+                else:
+                    chars.append(c)
+                    num = substr_to_num(chars)
+                    if num == -1:
+                        continue
+
+                if first == -1:
+                    first = num
+                last = num
+            return first * 10 + last
+
+        with open(
+            self.filepath, "r", encoding=sys.getdefaultencoding()
+        ) as file:
+            return sum(line_to_num(line) for line in file)
+
+    def part_1(self):
+        """Part 1 solver."""
+        return self.general_solve(False)
+
+    def part_2(self):
+        """Part 2 solver."""
+        return self.general_solve(True)
+
+    def solve(self) -> None:
+        """Runs part 1 and part 2."""
+        print("Day 1")
+        print(f"Solving: {self.filepath}")
+        print("Part 1:")
+        print(self.part_1())
+        print("---")
+        print("Part 2:")
+        print(self.part_2())
+
+
+if __name__ == "__main__":
+    match sys.argv:
+        case [_]:
+            solver = Solver()
+        case [_, "--path", path] | [_, "-p", path] | [_, path]:
+            solver = Solver(path)
+        case other:
+            print("Usage: python solution.py <path>")
+            sys.exit(0)
+    solver.solve()
