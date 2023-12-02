f89df3be5b8e8fc00d32e54a9e6b547f48c99666
Keith Dao <keith.dao.248@gmail.com>
2023-12-02 17:36:38 +1000

    Reformats day 1 c++ solver.

--- Day 1/solution.cpp
+++ Day 1/solution.cpp
@@ -2,63 +2,79 @@
 #include <fstream>
 #include <iostream>
 
-struct TrieNode {
-	TrieNode *children[26] {nullptr};
+struct TrieNode
+{
+	TrieNode *children[26]{nullptr};
 	int val = -1;
 
-	~TrieNode() {
-		for (int i = 0; i < 26; ++i) {
-			if (this->children[i] != nullptr) {
+	~TrieNode()
+	{
+		for (int i = 0; i < 26; ++i)
+		{
+			if (this->children[i] != nullptr)
+			{
 				delete this->children[i];
 			}
 		}
 	}
 
-	void set(char c) {
+	void set(char c)
+	{
 		int i = c - 'a';
-		if (this->children[i] == nullptr) {
+		if (this->children[i] == nullptr)
+		{
 			this->children[i] = new TrieNode();
 		}
 	}
 
-	bool contains(char c) {
+	bool contains(char c)
+	{
 		return this->children[c - 'a'] != nullptr;
 	}
 
-	TrieNode* get(char c) {
+	TrieNode *get(char c)
+	{
 		this->set(c);
 		return this->children[c - 'a'];
 	}
 };
 
-class Solver {
+class Solver
+{
 	std::filesystem::path filepath;
-	TrieNode* root;
-	
-	int substr_to_num(std::string substr) {
+	TrieNode *root;
+
+	int substr_to_num(std::string substr)
+	{
 		TrieNode *curr = this->root;
-		for (int i = substr.size() - 1; i >= 0; --i) {
-			if (!curr->contains(substr[i])) {
+		for (int i = substr.size() - 1; i >= 0; --i)
+		{
+			if (!curr->contains(substr[i]))
+			{
 				break;
 			}
 			curr = curr->get(substr[i]);
 
-			if (curr->val != -1) {
+			if (curr->val != -1)
+			{
 				return curr->val;
 			}
 		}
 		return -1;
 	}
 
-	int general_solver(bool include_word_numbers=false) {
+	int general_solver(bool include_word_numbers = false)
+	{
 		std::ifstream file;
 		file.open(this->filepath);
 		int result = 0, first = -1, last = -1;
 		std::string substr = "";
 
-		while (!file.eof()) {
+		while (!file.eof())
+		{
 			char c = file.get();
-			if (c == '\n' || c == EOF) {
+			if (c == '\n' || c == EOF)
+			{
 				result += first * 10 + last;
 				first = last = -1;
 				substr.clear();
@@ -66,18 +82,25 @@ class Solver {
 			}
 
 			int num;
-			if (std::isdigit(c)) {
+			if (std::isdigit(c))
+			{
 				num = c - '0';
 				substr.clear();
-			} else if (!include_word_numbers) {
+			}
+			else if (!include_word_numbers)
+			{
 				continue;
-			} else {
+			}
+			else
+			{
 				substr.push_back(c);
 				num = this->substr_to_num(substr);
-				if (num == -1) continue;
+				if (num == -1)
+					continue;
 			}
 
-			if (first == -1) first = num;
+			if (first == -1)
+				first = num;
 			last = num;
 		}
 
@@ -85,34 +108,40 @@ class Solver {
 		return result;
 	}
 
-	int part_1() {
+	int part_1()
+	{
 		return this->general_solver(false);
 	}
 
-	int part_2() {
+	int part_2()
+	{
 		return this->general_solver(true);
 	}
 
 public:
-	Solver(std::filesystem::path filepath = "input.txt") : filepath(filepath) {
+	Solver(std::filesystem::path filepath = "input.txt") : filepath(filepath)
+	{
 		this->root = new TrieNode();
-		std::string words[9] {
-			"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
-		};
-		for (int num = 0; num < 9; ++num) {
-			TrieNode* curr = this->root;
-			for (int i = words[num].size() - 1; i >= 0; --i) {
+		std::string words[9]{
+				"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+		for (int num = 0; num < 9; ++num)
+		{
+			TrieNode *curr = this->root;
+			for (int i = words[num].size() - 1; i >= 0; --i)
+			{
 				curr = curr->get(words[num][i]);
 			}
 			curr->val = num + 1;
 		}
 	};
 
-	~Solver() {
+	~Solver()
+	{
 		delete this->root;
 	}
 
-	void solve() {
+	void solve()
+	{
 		std::cout << "Day 1" << std::endl;
 		std::cout << "Solving: " << this->filepath << std::endl;
 		std::cout << "Part 1:" << std::endl;
@@ -123,15 +152,23 @@ public:
 	}
 };
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 	std::unique_ptr<Solver> solver;
-	if (argc == 1) {
+	if (argc == 1)
+	{
 		solver = std::make_unique<Solver>();
-	} else if (argc == 2) {
+	}
+	else if (argc == 2)
+	{
 		solver = std::make_unique<Solver>(argv[1]);
-	} else if (argc == 3 && (argv[1] == "-p" || argv[1] == "--path")) {
+	}
+	else if (argc == 3 && (argv[1] == "-p" || argv[1] == "--path"))
+	{
 		solver = std::make_unique<Solver>(argv[2]);
-	} else {
+	}
+	else
+	{
 		return 1;
 	}
 
