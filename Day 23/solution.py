89520fa69c95a88138f40919f6b28cb62bfcf9ae
Keith Dao <keith.dao.248@gmail.com>
2023-12-23 20:05:19 +1000

    Refactors day 23 python solver to remove repeated patterns.

--- Day 23/solution.py
+++ Day 23/solution.py
@@ -1,10 +1,9 @@
 """
 Day 23 solution.
 """
+import collections
 import itertools
 import sys
-
-sys.setrecursionlimit(10000)
 
 
 class Solver:
@@ -13,132 +12,112 @@ class Solver:
     def __init__(self, filepath: str = "input.txt"):
         self.filepath = filepath
 
-    def part_1(self) -> int:
-        """Part 1 solver."""
+    def get_graph(
+        self, ignore_slopes: bool
+    ) -> tuple[
+        dict[tuple[int, int], dict[tuple[int, int], int]], tuple[int, int]
+    ]:
+        """
+        Parse the file and get a compressed graph with the number of rows
+        and columns respectively.
+        """
         with open(
             self.filepath, "r", encoding=sys.getdefaultencoding()
         ) as file:
-            graph = [line.strip() for line in file]
+            grid = [line.strip() for line in file]
 
-        def dfs(i, j, visited) -> int:
-            if i == len(graph) - 1:
-                return len(visited) - 1
-
-            n_i, n_j = i, j
-            if graph[i][j] == ">":
-                n_i, n_j = i, j + 1
-            elif graph[i][j] == "<":
-                n_i, n_j = i, j - 1
-            elif graph[i][j] == "^":
-                n_i, n_j = i - 1, j
-            elif graph[i][j] == "v":
-                n_i, n_j = i + 1, j
-
-            if i != n_i or j != n_j:
-                if (
-                    not 0 <= n_i < len(graph)
-                    or not 0 <= n_j < len(graph[0])
-                    or graph[n_i][n_j] == "#"
-                    or (n_i, n_j) in visited
-                ):
-                    return 0
-
-                visited.add((n_i, n_j))
-                result = dfs(n_i, n_j, visited)
-                visited.remove((n_i, n_j))
-                return result
-
-            result = 0
-            for d_i, d_j in itertools.pairwise((0, -1, 0, 1, 0)):
-                n_i, n_j = i + d_i, j + d_j
-                if (
-                    not 0 <= n_i < len(graph)
-                    or not 0 <= n_j < len(graph[0])
-                    or graph[n_i][n_j] == "#"
-                    or (n_i, n_j) in visited
-                ):
-                    continue
-
-                visited.add((n_i, n_j))
-                result = max(result, dfs(n_i, n_j, visited))
-                visited.remove((n_i, n_j))
-            return result
-
-        return dfs(0, 1, {(0, 1)})
-
-    def part_2(self) -> int:
-        """Part 2 solver."""
-        with open(
-            self.filepath, "r", encoding=sys.getdefaultencoding()
-        ) as file:
-            blocks = [line.strip() for line in file]
-
-        m, n = len(blocks), len(blocks[0])
-        junctions: list[tuple[int, int]] = []
-        for i, row in enumerate(blocks):
+        slopes = "<^>v"
+        m, n = len(grid), len(grid[0])
+        graph: dict[
+            tuple[int, int], dict[tuple[int, int], int]
+        ] = collections.defaultdict(dict)
+        for i, row in enumerate(grid):
             for j, x in enumerate(row):
                 if x == "#":
                     continue
 
-                if (
-                    sum(
-                        0 <= (n_i := i + d_i) < m
-                        and 0 <= (n_j := j + d_j) < n
-                        and blocks[n_i][n_j] != "#"
-                        for d_i, d_j in itertools.pairwise((0, -1, 0, 1, 0))
-                    )
-                    > 2
+                for slope_type, (d_i, d_j) in zip(
+                    slopes, itertools.pairwise((0, -1, 0, 1, 0))
                 ):
-                    junctions.append((i, j))
+                    if (
+                        not ignore_slopes
+                        and grid[i][j] in slopes
+                        and grid[i][j] != slope_type
+                    ):
+                        continue
 
-        junctions.extend([(0, 1), (len(blocks) - 1, len(blocks[-1]) - 2)])
-        graph: list[list[dict[tuple[int, int], int]]] = [
-            [dict() for _ in row] for row in blocks
-        ]
-        for node in junctions:
-            visited = [[False] * len(row) for row in blocks]
-            stack: list[tuple[int, int, int]] = [(*node, 0)]
-            while stack:
-                i, j, steps = stack.pop()
-                if visited[i][j]:
-                    continue
-                visited[i][j] = True
-
-                if (i, j) in junctions and (i, j) != node:
-                    graph[node[0]][node[1]][i, j] = steps
-                    continue
-
-                for d_i, d_j in itertools.pairwise((0, -1, 0, 1, 0)):
                     n_i, n_j = i + d_i, j + d_j
                     if (
                         not 0 <= n_i < m
                         or not 0 <= n_j < n
-                        or blocks[n_i][n_j] == "#"
+                        or grid[n_i][n_j] == "#"
                     ):
                         continue
 
-                    stack.append((n_i, n_j, steps + 1))
+                    graph[i, j][n_i, n_j] = 1
 
+        done = False
+        while not done:
+            done = True
+            for node, neighbours in graph.items():
+                if len(neighbours) != 2:
+                    continue
+
+                a, b = neighbours
+                try:
+                    graph[a].pop(node)
+                    graph[a][b] = neighbours[a] + neighbours[b]
+                except KeyError:
+                    # Edge is unidirectional i.e. only node -> a
+                    pass
+
+                try:
+                    graph[b].pop(node)
+                    graph[b][a] = neighbours[a] + neighbours[b]
+                except KeyError:
+                    # Edge is unidirectional i.e. only node -> b
+                    pass
+
+                graph.pop(node)
+                done = False
+                break
+
+        return graph, (m, n)
+
+    def generic_solve(self, ignore_slopes: bool) -> int:
+        """Generic solve."""
+        graph, (m, n) = self.get_graph(ignore_slopes)
+
+        stack: list[tuple[int, int, int]] = [(0, 1, 0)]
+        visited: list[list[bool]] = [[False] * n for _ in range(m)]
         result = 0
-        visited = [[False] * len(row) for row in blocks]
+        while stack:
+            i, j, total = stack.pop()
+            if total == -1:
+                visited[i][j] = False
+                continue
 
-        def dfs(i: int, j: int, count: int) -> None:
-            nonlocal result
-
-            if visited[i][j]:
-                return
-
-            if i == len(blocks) - 1:
-                result = max(result, count)
-                return
+            if i == m - 1:
+                result = max(result, total)
+                continue
 
             visited[i][j] = True
-            for next_, cost in graph[i][j].items():
-                dfs(*next_, count + cost)
-            visited[i][j] = False
+            stack.append((i, j, -1))
+            for (n_i, n_j), cost in graph[i, j].items():
+                if visited[n_i][n_j]:
+                    continue
 
-        dfs(0, 1, 0)
+                stack.append((n_i, n_j, total + cost))
+
         return result
+
+    def part_1(self) -> int:
+        """Part 1 solver."""
+        return self.generic_solve(False)
+
+    def part_2(self) -> int:
+        """Part 2 solver."""
+        return self.generic_solve(True)
 
     def solve(self) -> None:
         """Runs part 1 and part 2."""
